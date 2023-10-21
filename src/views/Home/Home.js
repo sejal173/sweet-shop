2fa3e60cb610a898eb242ef8bd56f1f0aeeb6081
sejal173 <sejalkakade123@gmail.com>
2023-10-21 12:10:19 +0530

    sweets added card

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,23 +1,69 @@
 import "./Home.css";
+import React from "react";
 import Config from "./../../utills/Config.json";
 
 console.log(Config);
-import React from 'react'
 
 export default function Home() {
-
   return (
     <>
-       <div>
-           <h1 className="shop-heading"  style={{color:Config.theme.orangeColor}}>{Config.shopname}</h1>
-           <img src="https://mirchi.com/os/cdn/content/images/Sweets_mobile_273039.jpg" className="header-img"/>
+      <h1 className="shop-heading text-center">{Config.shopname}</h1>
+      <img src={Config.img} className="header-img" />
+      {/* <h3 className="description-container scrolling-content">{Config.description}</h3> */}
+      <div>
+        <h1 className="shop-heading text-center">Selections:</h1>
 
-           <div className="description-container">
-                  <h3 className="scrolling-content">{Config.description}</h3>  
-           </div>
+        <div className="display-flex">
+          {Config.Selection.map((selectiondata, index) => {
+            const { description, price, quality, name } = selectiondata;
 
-           
-       </div>
+            return (
+              <>
+                <div className="flex-container">
+                  <div
+                    className="selection-card"
+                    style={{ backgroundColor: Config.theme.secondaryColor }}
+                  >
+                    <h3 className="text-center">{name}</h3>
+                    <h4 style={{ color: Config.theme.orangeColor }}>
+                      {description}
+                    </h4>
+                    <h2>Price: {price}</h2>
+                    <h1>quality:{quality}</h1>
+                  </div>
+                </div>
+              </>
+            );
+          })}
+        </div>
+
+        <h1 className="shop-heading text-center">Services :</h1>
+
+        {/* {Config.services.map((servicesData , index) => {
+                 {Object.values(servicesData)[0]} 
+            })}  */}
+
+<div className="display-flex">
+        {Config.services.map((servicesData, index) => {
+          const { img, product_name, description, price, button } =
+            servicesData;
+          return (
+            <>
+              <div
+                className="services-card"
+                style={{ backgroundColor: Config.theme.secondaryColor }}
+              >
+                <img src={img} className="food-img" />
+                <h3>{price}</h3>
+                <p>{product_name}</p>
+                <p>{description}</p>
+                <button> {button}</button>
+              </div>
+            </>
+          );
+        })}
+        </div>
+      </div>
     </>
-  )
+  );
 }
